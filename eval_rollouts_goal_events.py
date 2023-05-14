7f5d98b18dc4f36ccc7a41c832d46c248c889b76
Logan Cross <locross93@gmail.com>
2023-05-14 16:28:59 -0700

    Minor

--- eval_rollouts_goal_events.py
+++ eval_rollouts_goal_events.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
         'rssm_disc': {'class': DreamerV2, 'config': 'rssm_disc_default_config.json', 
                       'model_dir': 'rssm_disc', 'epoch': '1000', 'model_label': 'RSSM Discrete'},
         'transformer': {'class': TransformerMSPredictor, 'config': 'transformer_default_config.json', 
-                      'model_dir': 'transformer_mp', 'epoch': '500', 'model_label': 'Transformer MP'},
+                      'model_dir': 'transformer_mp', 'model_label': 'Transformer MP'},
         }
     #keys2analyze = ['rssm_discrete', 'multistep_predictor']
     keys2analyze = ['transformer']
