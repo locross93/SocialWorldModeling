3cdad86d761786fac683dece1906f278591c8de0
Violet Xiang <ziyxiang@node2-ccn2cluster.stanford.edu>
2023-06-02 20:26:38 -0700

    add transformer norm and vel

--- visualize_model_rollouts.py
+++ visualize_model_rollouts.py
@@ -17,8 +17,7 @@ import matplotlib.pyplot as plt
 import moviepy.editor as mpy
 from moviepy.video.io.bindings import mplfig_to_npimage
 
-from analysis_utils import init_model_class, get_data_columns, inverse_normalize, \
-    load_trained_model
+from analysis_utils import load_trained_model, get_data_columns, inverse_normalize
 from annotate_pickup_timepoints import annotate_pickup_timepoints
 from annotate_goal_timepoints import annotate_goal_timepoints
 
@@ -45,7 +44,6 @@ def load_args():
     parser.add_argument('--model_key', type=str, required=True, help='Model to use for visualization')
     parser.add_argument('--train_or_val', type=str, default='val', help='Training or Validation Set')
     parser.add_argument('--device', type=str, default='cpu', help='Device')
-    parser.add_argument('--gnn_model', type=bool, default=False, help='GNN Model')
     # which trial to visualize
     parser.add_argument('--trial_type', type=str, default='single_goal', help='Trial Type') # single_goal, multi_goal, all
     parser.add_argument('--trial_num', type=int, default=0, help='Trial Type')
@@ -84,10 +82,10 @@ def make_traj_subplots(x_true, x_pred, subplot_dims, steps, save_file, data_colu
             x_ind = data_columns.index('obj'+str(obj_num)+'_x')
             z_ind = data_columns.index('obj'+str(obj_num)+'_z')
             ax[row, col].plot(x_true[step,x_ind], x_true[step,z_ind], marker='o', markerfacecolor=color, markeredgecolor='black')
-        for num_agent in range(2):
-            color = colors[num_agent+3]
-            x_ind = data_columns.index('agent'+str(num_agent)+'_x')
-            z_ind = data_columns.index('agent'+str(num_agent)+'_z')
+        for agent_num in range(2):
+            color = colors[agent_num+3]
+            x_ind = data_columns.index('agent'+str(agent_num)+'_x')
+            z_ind = data_columns.index('agent'+str(agent_num)+'_z')
             ax[row, col].plot(x_true[step,x_ind], x_true[step,z_ind], marker='^', markersize=16, markerfacecolor=color, markeredgecolor='black')
         # observer
         ax[row, col].plot(0.0, -6.0, marker='s', markersize=12)
@@ -98,10 +96,10 @@ def make_traj_subplots(x_true, x_pred, subplot_dims, steps, save_file, data_colu
             x_ind = data_columns.index('obj'+str(obj_num)+'_x')
             z_ind = data_columns.index('obj'+str(obj_num)+'_z')
             ax[row, col].plot(x_pred[step,x_ind], x_pred[step,z_ind], marker='o', markerfacecolor=color, markeredgecolor=color, alpha=0.5)
-        for num_agent in range(2):
-            color = colors[num_agent+3]
-            x_ind = data_columns.index('agent'+str(num_agent)+'_x')
-            z_ind = data_columns.index('agent'+str(num_agent)+'_z')
+        for agent_num in range(2):
+            color = colors[agent_num+3]
+            x_ind = data_columns.index('agent'+str(agent_num)+'_x')
+            z_ind = data_columns.index('agent'+str(agent_num)+'_z')
             ax[row, col].plot(x_pred[step,x_ind], x_pred[step,z_ind], marker='^', markersize=16, markerfacecolor=color, markeredgecolor=color, alpha=0.5)
     # save
     plt.savefig(save_file, dpi=300)
@@ -126,9 +124,9 @@ def make_frame_compare(t):
         x_ind = data_columns.index('obj'+str(obj_num)+'_x')
         z_ind = data_columns.index('obj'+str(obj_num)+'_z')
         ax[0].plot(x_true[t_ind,x_ind], x_true[t_ind,z_ind], marker='o')
-    for num_agent in range(2):
-        x_ind = data_columns.index('agent'+str(num_agent)+'_x')
-        z_ind = data_columns.index('agent'+str(num_agent)+'_z')
+    for agent_num in range(2):
+        x_ind = data_columns.index('agent'+str(agent_num)+'_x')
+        z_ind = data_columns.index('agent'+str(agent_num)+'_z')
         ax[0].plot(x_true[t_ind,x_ind], x_true[t_ind,z_ind], marker='^', markersize=16)
     # observer
     ax[0].plot(0.0, -6.0, marker='s', markersize=12)
@@ -140,9 +138,9 @@ def make_frame_compare(t):
         x_ind = data_columns.index('obj'+str(obj_num)+'_x')
         z_ind = data_columns.index('obj'+str(obj_num)+'_z')
         ax[1].plot(x_pred[t_ind,x_ind], x_pred[t_ind,z_ind], marker='o')
-    for num_agent in range(2):
-        x_ind = data_columns.index('agent'+str(num_agent)+'_x')
-        z_ind = data_columns.index('agent'+str(num_agent)+'_z')
+    for agent_num in range(2):
+        x_ind = data_columns.index('agent'+str(agent_num)+'_x')
+        z_ind = data_columns.index('agent'+str(agent_num)+'_z')
         ax[1].plot(x_pred[t_ind,x_ind], x_pred[t_ind,z_ind], marker='^', markersize=16)
     ax[1].plot(0.0, -6.0, marker='s', markersize=12)
     ax[1].set_title('Reconstructed Trajectory')
@@ -169,8 +167,13 @@ def make_frame_compare(t):
     return mplfig_to_npimage(fig)
 
 if __name__ == '__main__':
-    args = load_args()   
-    """Load data"""
+    args = load_args()
+    
+    model_info = MODEL_DICT_VAL[args.model_key]
+    model_name = model_info['model_label']
+    model = load_trained_model(model_info, args)
+    
+    # load data
     dataset_file = os.path.join(args.data_dir, args.dataset)
     loaded_dataset = pickle.load(open(dataset_file, 'rb'))
     train_dataset, test_dataset = loaded_dataset
@@ -178,7 +181,7 @@ if __name__ == '__main__':
         input_data = train_dataset.dataset.tensors[0][train_dataset.indices,:,:]
     else:
         input_data = test_dataset.dataset.tensors[0][test_dataset.indices,:,:]
-    
+        
     if args.dataset in DATASET_NUMS and DATASET_NUMS[args.dataset] == 1:
         # first dataset use states to define events
         # select trajectory where goal occurred
@@ -203,13 +206,21 @@ if __name__ == '__main__':
             # data is normalize, project it back into regular input space
             min_values, max_values = exp_info_dict['min_max_values']
             velocity = any('vel' in s for s in data_columns)
-            input_data = inverse_normalize(input_data, max_values.astype(np.float32), min_values.astype(np.float32), velocity)    
-    # select trial  
+            input_data = inverse_normalize(input_data, max_values.astype(np.float32), min_values.astype(np.float32), velocity)
+    
+    # input_data = input_data.numpy()
+    # min_values = np.min(input_data, axis=(0, 1))
+    # max_values = np.max(input_data, axis=(0, 1))
+    # ranges = max_values - min_values
+    # normalized_tensor = (input_data - min_values) / ranges
+    # input_data = inverse_normalize(normalized_tensor, max_values, min_values)
+    # input_data = torch.tensor(input_data)
+            
     if args.trial_type == 'single_goal':
         traj_ind = single_goal_trajs[args.trial_num]
         steps2pickup = np.max(pickup_timepoints[traj_ind,:]).astype(int)
         burn_in_length = steps2pickup
-    elif args.trial_type == 'multi_goal':        
+    elif args.trial_type == 'multi_goal':
         traj_ind = multi_goal_trajs[args.trial_num]
         goal_num = args.goal_num
         steps2pickup = np.sort(pickup_timepoints[traj_ind,:])[goal_num].astype(int)
@@ -220,27 +231,15 @@ if __name__ == '__main__':
         burn_in_length = args.burn_in_length
     
     rollout_length = input_data.size(1) - burn_in_length
-    print(f"burn_in_length: {burn_in_length}, traj_ind: {traj_ind}, rollout_length: {rollout_length}")
-    # load model        
-    model_info = MODEL_DICT_VAL[args.model_key]
-    config = load_config(os.path.join(args.model_config_dir, model_info['config']))
-    model_name = model_info['model_label']    
-    if args.model_key == "agent_former":
-        config['past_frames'] = burn_in_length
-        config['future_frames'] = rollout_length
-    # @TODO this needs to be updated for AgentFormer
-    model = load_trained_model(model_info, args)
-        
-    # visualize
     x = input_data[traj_ind,:,:].unsqueeze(0)
     if x.dtype == torch.float64:
         x = x.float()
     x_true = x[0,:burn_in_length+rollout_length,:].numpy()
-    x_pred = x_true.copy()    
-    if args.model_key == 'transformer_wm':
-        rollout_x = model.variable_length_rollout(x, steps2pickup, rollout_length).cpu().detach()
-    else:        
-        rollout_x = model.forward_rollout(x, burn_in_length, rollout_length).cpu().detach().numpy()
+    x_pred = x_true.copy()
+    # if args.model_key == 'transformer_wm':
+    #     rollout_x = model.variable_length_rollout(x, steps2pickup, rollout_length).cpu().detach()
+    # else:
+    rollout_x = model.forward_rollout(x, burn_in_length, rollout_length).cpu().detach().numpy()
     x_pred[burn_in_length:,:] = rollout_x 
     
     viz_dir = os.path.join(args.analysis_dir, 'results/viz_trajs',args.model_key)
