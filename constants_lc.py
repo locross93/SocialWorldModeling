eb9b53eac256edfbd0ffc3becc1d79b6c6cc78fe
Logan Cross <locross93@gmail.com>
2023-07-09 22:59:17 -0700

    Minor

--- constants_lc.py
+++ constants_lc.py
@@ -99,6 +99,8 @@ MODEL_DICT_VAL=  {
         'model_dir': 'transformer_iris_concat_pos_embd_lr1e-4', 'model_label': 'Transformer Iris Concat Pos Embd lr1e-4'},
 }
 DEFAULT_VALUES = {
+	'train_seed': 911320,
+    'eval_seed': 911320, #834869, #(good for SGNet & RSSM Discrete) 
     'analysis_dir': '/Users/locro/Documents/Stanford/SocialWorldModeling/' if platform.system() == 'Windows' else  '/home/locross/SocialWorldModeling/',
     'data_path': '/Users/locro/Documents/Stanford/analysis/data/swm_data.pkl' if platform.system() == 'Windows' else  '/data2/locross/data/swm_data.pkl',
     'data_dir': '/Users/locro/Documents/Stanford/analysis/data/' if platform.system() == 'Windows' else  '/data2/locross/data/',
