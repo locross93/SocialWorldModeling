bd2dffc3fb5feebf2e181b7c02b749f0fe3f3e6c
Logan Cross <locross93@gmail.com>
2023-07-09 22:51:34 -0700

    New save paths

--- gnn_models/gat.py
+++ gnn_models/gat.py
@@ -78,7 +78,6 @@ class GAT(nn.Module):
 
     def forward(self, human_states, batch_graph):
         # encoder
-        breakpoint()
         embeddings = self.embedding(human_states)
         # dynamical
         normalized_A, next_H = self.dynamical(embeddings)
